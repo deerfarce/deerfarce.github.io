b0d9a7d088f8478b2d573c9e11a090cfb437136e
deerfarce <7387712+deerfarce@users.noreply.github.com>
2024-12-27 19:58:38 -0500

    setting up fx stuff

---
+++ etc/ce/ce_emote-fx-button.js
@@ -0,0 +1,82 @@
+//for use with cytubeEnhanced/billtube
+
+function showEmoteFxModal() {
+  let fxmodal = createModal({title:"Emote Effects", wrap_id:"chat_fx_list"})
+  $(fxmodal).find(".modal-body").html(`
+    <ul>
+      <li>
+        <code>fxSpin</code> - makes emotes spin around.
+      </li>
+      <li>
+        <code>fxBounce</code> - makes emotes bounce.
+      </li>
+      <li>
+        <code>fxGlow</code> - gives emotes a green glow.
+      </li>
+      <li>
+        <code>fxBw</code> - makes emotes black & white.
+      </li>
+      <li>
+        <code>fxShrink</code> - causes emotes to shrink in size over time.
+      </li>
+      <li>
+        <code>fxPop</code> - causes emotes to grow and then "pop".
+      </li>
+      <li>
+        <code>fxWide</code> - makes emotes wide.
+      </li>
+      <li>
+        <code>fxRed</code> - makes emotes red.
+      </li>
+      <li>
+        <code>fxGreen</code> - makes emotes green.
+      </li>
+      <li>
+        <code>fxDespair</code> - D E S P A I R
+      </li>
+      <li>
+        <code>fxPov</code> - gives emotes a perspective effect.
+      </li>
+      <li>
+        <code>fxRainbow</code> - causes emotes to rapidly change color.
+      </li>
+      <li>
+        <code>fxShake</code> - causes emotes to shake.
+      </li>
+      <li>
+        <code>fxShake2</code> - causes emotes to shake.
+      </li>
+      <li>
+        <code>fxShake3</code> - causes emotes to shake violently.
+      </li>
+      <li>
+        <code>fxJam</code> - gives emotes a jam animation effect.
+      </li>
+      <li>
+        <code>fxFadeout</code> - causes emotes to slowly fade out.
+      </li>
+    </ul>`);
+  fxmodal.modal();
+}
+    
+    
+(function() {
+    if ($("#chat-fx-btn").length <= 0) {
+        let btn = $("<button/>", {
+          class: "chatbtn",
+          id: "chat-fx-btn",
+          "data-tooltip": "Emote FX",
+          "data-tooltip-pos": "up",
+          click: function() {
+            showEmoteFxModal();
+          }
+        });
+        
+        btn.append($("<i/>", {
+          class: "material-icons",
+          text: "auto_fix_high"
+        }));
+        btn.appendTo($("#chat-controls"));
+    }
+})();
+
--- etc/cytube-emote-limiter.js
+++ etc/cytube-emote-limiter.js
@@ -1,19 +1,29 @@
 //this purposefully iterates past the emote limit to blank out any excessive emote text, so it's not efficient - but it does reduce text spam
 
-if (!window.hasOwnProperty("EMOTESPERLINE")) {
+if (!window.hasOwnProperty("EMOTESPERLINE") || !window.hasOwnProperty("FXPERLINE")) {
   window["EMOTESPERLINE"] = 3;
+  window["FXPERLINE"] = 9;
 }
 
 window.execEmotesEfficient = function(e){
   var c=0;
+  var d=0;
   return CHANNEL.badEmotes.forEach(function(t){
     e=e.replace(t.regex, function(match, cont) {
+      if (/^(fx|ov)[A-Z0-9]/.test(t.name)) {
+        if (window["DISABLE_EMOTE_FX"]) return '';
+        return (d++>=window["FXPERLINE"] ? '' : (cont+'<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">'))
+      }
       return (c++>=window["EMOTESPERLINE"] ? '' : (cont+'<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">'))
     })
   }),
   e=e.replace(/[^\s]+/gi,function(e){
     if (CHANNEL.emoteMap.hasOwnProperty(e)){
       var t=CHANNEL.emoteMap[e];
+      if (/^(fx|ov)[A-Z0-9]/.test(t.name)) {
+        if (window["DISABLE_EMOTE_FX"]) return '';
+        return (d++>=window["FXPERLINE"] ? '' : '<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">');
+      }
       return (c++>=window["EMOTESPERLINE"] ? '' : '<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">');
     }
     return e
@@ -22,10 +32,15 @@ window.execEmotesEfficient = function(e){
 
 window.execEmotes = function(e){
   var c=0;
+  var d=0;
   return (USEROPTS.no_emotes || window["EMOTESPERLINE"] === 0) ? e :
     (CyTube.featureFlag && CyTube.featureFlag.efficientEmotes) ? execEmotesEfficient(e) :
       (CHANNEL.emotes.forEach(function(t){
         e=e.replace(t.regex, function(match, cont) {
+          if (/^(fx|ov)[A-Z0-9]/.test(t.name)) {
+            if (window["DISABLE_EMOTE_FX"]) return '';
+            return (d++>=window["FXPERLINE"] ? '' : (cont+'<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">'))
+          }
           return (c++>=window["EMOTESPERLINE"] ? '' : (cont+'<img class="channel-emote" src="'+t.image+'" title="'+t.name+'">'))
         })
       }) ,e)
